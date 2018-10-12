66a13d7bbc74ff8570f1b587deb8026308ffd4f4
Angel Contreras Torres <angel@makingdevs.com>
2018-10-12 10:06:25 -0500

    Implementando LogInterceptor

--- src/main/groovy/com/pegaso/ws/endpoint/impl/HelloImpl.groovy
+++ src/main/groovy/com/pegaso/ws/endpoint/impl/HelloImpl.groovy
@@ -2,6 +2,8 @@ package com.pegaso.ws.endpoint.impl
 
 import com.pegaso.ws.dto.BalanceQueryDto
 import com.pegaso.ws.endpoint.Hello
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 import javax.jws.WebService
@@ -11,12 +13,16 @@ import javax.jws.WebService
 @WebService(endpointInterface="com.pegaso.ws.endpoint.Hello")
 class HelloImpl implements Hello {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass())
+
 
     String saluda ( String name ){
          "Hola ${name}, ya quedo el ws...."
     }
 
     BalanceQueryDto getBalanceQuery( String pan ){
+        println "Hola desde println"
+        log.debug("Hola mi hermano")
         new BalanceQueryDto( code: 1, description: "Consulta realizada con éxito", availableAmount: 22.50 )
     }
 
---
+++ src/main/groovy/com/pegaso/ws/interceptor/LoggingOutInterceptor.groovy
@@ -0,0 +1,165 @@
+package com.pegaso.ws.interceptor
+
+import org.apache.cxf.helpers.IOUtils
+import org.apache.cxf.interceptor.AbstractLoggingInterceptor
+import org.apache.cxf.interceptor.Fault
+import org.apache.cxf.interceptor.LoggingMessage
+import org.apache.cxf.interceptor.StaxOutInterceptor
+
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.cxf.common.injection.NoJSR250Annotations;
+import org.apache.cxf.common.logging.LogUtils;
+import org.apache.cxf.io.CacheAndWriteOutputStream;
+import org.apache.cxf.io.CachedOutputStream;
+import org.apache.cxf.io.CachedOutputStreamCallback;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.Phase;
+
+/**
+ *
+ */
+@NoJSR250Annotations
+public class LoggingOutInterceptor extends AbstractLoggingInterceptor {
+    Logger LOG = LogUtils.getLogger(LoggingOutInterceptor.class);
+    String LOG_SETUP = LoggingOutInterceptor.class.getName() + ".log-setup";
+
+    public LoggingOutInterceptor(String phase) {
+        super(phase);
+        addBefore(StaxOutInterceptor.class.getName());
+    }
+    public LoggingOutInterceptor() {
+        this(Phase.PRE_STREAM);
+    }
+    public LoggingOutInterceptor(int lim) {
+        this();
+        limit = lim;
+    }
+
+    public LoggingOutInterceptor(PrintWriter w) {
+        this();
+        this.writer = w;
+    }
+
+
+    public void handleMessage(Message message) throws Fault {
+        final OutputStream os = message.getContent(OutputStream.class);
+        if (os == null) {
+            return;
+        }
+        Logger logger = getMessageLogger(message);
+        if (logger.isLoggable(Level.INFO) || writer != null) {
+            // Write the output while caching it for the log message
+            boolean hasLogged = message.containsKey(LOG_SETUP);
+            if (!hasLogged) {
+                message.put(LOG_SETUP, Boolean.TRUE);
+                final CacheAndWriteOutputStream newOut = new CacheAndWriteOutputStream(os);
+                message.setContent(OutputStream.class, newOut);
+                newOut.registerCallback(new LoggingCallback(logger, message, os));
+            }
+        }
+    }
+
+    class LoggingCallback implements CachedOutputStreamCallback {
+
+        private final Message message;
+        private final OutputStream origStream;
+        private final Logger logger; //NOPMD
+
+        public LoggingCallback(final Logger logger, final Message msg, final OutputStream os) {
+            this.logger = logger;
+            this.message = msg;
+            this.origStream = os;
+        }
+
+        public void onFlush(CachedOutputStream cos) {
+
+        }
+
+        public void onClose(CachedOutputStream cos) {
+            String id = (String)message.getExchange().get(LoggingMessage.ID_KEY);
+
+            org.apache.cxf.message.Message inMessage = (org.apache.cxf.message.Message)message.getExchange().getInMessage()
+            InputStream is = inMessage.getContent(InputStream.class);
+            InputStream out = message.getContent(InputStream.class)
+
+
+
+            if (id == null) {
+                id = LoggingMessage.nextId();
+                message.getExchange().put(LoggingMessage.ID_KEY, id);
+            }
+            final LoggingMessage buffer  = new LoggingMessage("Outbound Message\n---------------------------",
+                    id);
+
+            Integer responseCode = (Integer)message.get(Message.RESPONSE_CODE);
+            if (responseCode != null) {
+                buffer.getResponseCode().append(responseCode);
+            }
+
+            String encoding = (String)message.get(Message.ENCODING);
+
+            if (encoding != null) {
+                buffer.getEncoding().append(encoding);
+            }
+            String httpMethod = (String)message.get(Message.HTTP_REQUEST_METHOD);
+            if (httpMethod != null) {
+                buffer.getHttpMethod().append(httpMethod);
+            }
+            String address = (String)message.get(Message.ENDPOINT_ADDRESS);
+            if (address != null) {
+                buffer.getAddress().append(address);
+            }
+            String ct = (String)message.get(Message.CONTENT_TYPE);
+            if (ct != null) {
+                buffer.getContentType().append(ct);
+            }
+            Object headers = message.get(Message.PROTOCOL_HEADERS);
+            if (headers != null) {
+                buffer.getHeader().append(headers);
+            }
+
+            if (cos.getTempFile() == null) {
+                //buffer.append("Outbound Message:\n");
+                if (cos.size() > limit) {
+                    buffer.getMessage().append("(message truncated to " + limit + " bytes)\n");
+                }
+            } else {
+                buffer.getMessage().append("Outbound Message (saved to tmp file):\n");
+                buffer.getMessage().append("Filename: " + cos.getTempFile().getAbsolutePath() + "\n");
+                if (cos.size() > limit) {
+                    buffer.getMessage().append("(message truncated to " + limit + " bytes)\n");
+                }
+            }
+            try {
+                writePayload(buffer.getPayload(), cos, encoding, ct);
+            } catch (Exception ex) {
+                //ignore
+            }
+
+            String theString = IOUtils.toString(is, encoding);
+
+            log(logger, buffer.toString());
+            try {
+                //empty out the cache
+                cos.lockOutputStream();
+                cos.resetOut(null, false);
+            } catch (Exception ex) {
+                //ignore
+            }
+            message.setContent(OutputStream.class,
+                    origStream);
+        }
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return LOG;
+
+    }
+
+}
